79ae0a79a33ce722e1769a1c56707cd014d2ec91
pihuilong <pihuilong@qq.com>
2020-04-15 10:59:54 +0800

    feat: output burger ingredient dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) =>
+    [...Array(props.ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient key={igKey + i} type={igKey} />
+    )),
+  );
+  // eslint-disable-next-line no-console
+  console.log(transformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type='salad' />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-  // eslint-disable-next-line class-methods-use-this
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      meat: 2,
+      cheese: 2,
+    },
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build control</div>
       </React.Fragment>
     );
   }
-};
+}
 
 export default BurgerBuilder;
