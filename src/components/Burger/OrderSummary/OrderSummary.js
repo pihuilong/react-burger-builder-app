3919028ab1dedbcb42dc866e8a12381b248b299c
pihuilong <pihuilong@qq.com>
2020-04-18 23:38:42 +0800

    feat: create the orderSummary modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => (
+    <li key={igKey}>
+      {igKey}: {props.ingredients[igKey]}
+    </li>
+  ));
+  return (
+    <React.Fragment>
+      <h2>Your Order</h2>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+    </React.Fragment>
+  );
+};
+
+export default OrderSummary;
