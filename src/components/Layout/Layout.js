804a829c531ecbb419f83f3a5514307a34025894
pihuilong <pihuilong@qq.com>
2020-04-21 09:38:11 +0800

    chore: reusing the backDrop with sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 
 import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      <BurgerBuilder />
-    </main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  closeSideDrawerHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.closeSideDrawerHandler} />
+        <main className={classes.Content}>
+          <BurgerBuilder />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Layout;
