0e907ad6dc08f3fef65557ec5ed81df171c33027
pihuilong <pihuilong@qq.com>
2020-04-21 16:50:45 +0800

    improvement: improve performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => (
-    <li key={igKey}>
-      {igKey}: {props.ingredients[igKey]}
-    </li>
-  ));
-  return (
-    <React.Fragment>
-      <h2>Your Order</h2>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-  <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </React.Fragment>
-  );
-};
+class OrderSummary extends Component {
+  // NOTE: This could be a functional component, doesn't have to be a class.
+  componentWillUpdate() {
+    console.log('[OrderSummary] will update!');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => (
+        <li key={igKey}>
+          {igKey}: {this.props.ingredients[igKey]}
+        </li>
+      ),
+    );
+    return (
+      <React.Fragment>
+        <h2>Your Order</h2>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
 
 OrderSummary.propTypes = {
   price: PropTypes.number.isRequired,
   purchaseCancelled: PropTypes.func.isRequired,
   purchaseContinued: PropTypes.func.isRequired,
-}
+};
 
 export default OrderSummary;
